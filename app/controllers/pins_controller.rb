6445e306fe091f08f2c2f4e104e61b70e24a70c8
Julio Pérez <jperez@tegik.com>
2015-01-15 11:14:09 -0600

    PapperClip

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,55 @@
+class PinsController < ApplicationController
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index]
+
+  respond_to :html
+
+  def index
+    @pins = Pin.all
+    respond_with(@pins)
+  end
+
+  def show
+    respond_with(@pin)
+  end
+
+  def new
+    @pin = current_user.pins.build
+    respond_with(@pin)
+  end
+
+  def edit
+  end
+
+  def create
+    
+    @pin = current_user.pins.build(pin_params)
+    @pin.save
+    respond_with(@pin)
+  end
+
+  def update
+    @pin.update(pin_params)
+    respond_with(@pin)
+  end
+
+  def destroy
+    @pin.destroy
+    respond_with(@pin)
+  end
+
+  private
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+
+    def pin_params
+      params.require(:pin).permit(:description, :image)
+    end
+    
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "No estas autorizado para hacer esto" if @pin.nil?
+    end
+end
